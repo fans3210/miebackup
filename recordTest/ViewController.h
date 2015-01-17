14d5d594ad972287833ed36baacb24aa043ce6f8
yaodong <yaodong@vinova.sg>
2015-01-17 13:16:22 +0800

    Initial Commit

---
+++ recordTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  recordTest
+//
+//  Created by YAO DONG LI on 17/1/15.
+//  Copyright (c) 2015 ThreeStones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ recordTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  recordTest
+//
+//  Created by YAO DONG LI on 17/1/15.
+//  Copyright (c) 2015 ThreeStones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
